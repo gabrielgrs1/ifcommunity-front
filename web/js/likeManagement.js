16a7c7d921c87c21c1d99f9a2b8255c26a8fcd74
gabrielgrs1 <gabriel_guilherme2006@hotmail.com>
2018-07-05 13:03:59 -0300

    Servico de like deslike

--- web/js/likeManagement.js
+++ web/js/likeManagement.js
@@ -1,59 +1,47 @@
+habilitaLikeDeslike();
+
+
 function habilitaLikeDeslike(){
-    $('#body-principal a.botao-curtida > i').click(function(){
-        var switchLikeDeslike = $(this).parent('a').siblings().children('i').attr('id');
+    $("#body-principal a.botao-curtida > i").click(function(){
+        const switchLikeDeslike = $(this).parent("a").siblings().children("i").attr("id");
+        const usuario = $(jQuery.parseJSON($.session.get("usuario")));
+        const idAuthor = usuario["0"].userId;
+        const idPost = $(this).closest("li").attr("id");
+        let isLike = 0;
+        let isExclude = 0;
 
-        var idPost = $(this).closest('li').attr('id');
-        // Qual o default? Tipo, pra tirar tanto o curtir quanto o descurtir?
-        // Porque quando o cara tiver like e clicar direto no deslike, tem que remover o deslike e colocar o like...
-        //Pensando assim, não entendi quando vou fazer delete, update e insert..
-        //Qualquer coisa me liga.
-        var isLike = 2;
-        var isExclude = 2;
-
-        var usuario = $(jQuery.parseJSON($.session.get('usuario')));
-        var idAuthor = usuario["0"].userId;
-
-        if($(this).hasClass('like') == true){
-            if($(this).attr('id') == 'true'){
-                $(this).attr('id', false);
+        if($(this).hasClass("like") === true){
+            isLike = 1;
+            if($(this).attr("id") === "true"){
+                $(this).attr("id", false);
                 isExclude = 1;
-                console.log(('tinha like e tirou'));
             } else {
-                if (switchLikeDeslike == 'true'){
-                    console.log(('deslike e like'));
-                    $(this).parent('a').siblings().children('i').attr('id', false);
+                if (switchLikeDeslike === "true"){
+                    $(this).parent("a").siblings().children("i").attr("id", false);
                 }
-                console.log(('like'));
-                isLike = 1;
-                $(this).attr('id', true);
             }
         }
 
-        if($(this).hasClass('deslike') == true){
-            if($(this).attr('id') == 'true'){
-                console.log(('tinha deslike e tirou'));
+        if($(this).hasClass("deslike") === true){
+            if($(this).attr("id") === "true"){
                 isExclude = 1;
-                $(this).attr('id', false);
+                $(this).attr("id", false);
             } else {
-                if (switchLikeDeslike == 'true'){
-                    console.log(('like e deslike'));
-                    $(this).parent('a').siblings().children('i').attr('id', false);
+                if (switchLikeDeslike === "true"){
+                    $(this).parent("a").siblings().children("i").attr("id", false);
                 }
-                console.log(('deslike'));
-                isLike = 0;
-                $(this).attr('id', true);
+                $(this).attr("id", true);
             }
         }
 
-        // Está comentado pra não ficar chamando banco a toa.
-        // ajaxToLikeDeslike(idPost, idAuthor, isExclude, isLike);
+        ajaxToLikeDeslike(idPost, idAuthor, isExclude, isLike);
     });
 }
 
 function ajaxToLikeDeslike(idPost, idAuthor, isExclude, isLike) {
     $.ajax({
         url: "https://ifcommunity.herokuapp.com/post/like",
-        type: 'post',
+        type: "post",
         contentType: "application/json",
         crossDomain: true,
         data: JSON.stringify({
@@ -67,10 +55,11 @@ function ajaxToLikeDeslike(idPost, idAuthor, isExclude, isLike) {
         }
     })
         .done(function (critica) {
+            M.toast({html: critica, classes: "green"});
 
         })
         .fail(function (jqXHR, textStatus, postagem) {
-            M.toast({html: 'Erro ao reagir à postagem, contate um administrador!', classes: 'red'});
+            M.toast({html: "Erro ao reagir à postagem, contate um administrador!", classes: "red"});
             $(".preloader-wrapper").hide();
             if (jqXHR["status"] === 500) {
                 console.log("Erro 500, não foi possível estabelecer conexão com o servidor!");
--- web/js/login.js
+++ web/js/login.js
@@ -30,11 +30,6 @@ function mostrarSenha(inputSenha, botaoMostrar) {
     });
 }
 
-
-// validacaoFormulario("#login-cadastro", "#erro-usuario", /^[a-zA-Z0-9]([._](?![._])|[a-zA-Z0-9]){3,}[a-zA-Z0-9]$/, "Informe um usuário válido!");
-// validacaoFormulario("#senha-cadastro", "#erro-senha", /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,10}/, "Informe uma senha válida!");
-// validacaoFormulario("#email", "#erro-email", /(?:[a-z0-9!#$%&"*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&"*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/, "Informe um email válido!");
-
 function validaCPF(strCPF) {
     let Soma;
     let Resto;
